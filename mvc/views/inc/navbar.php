4bc74f2aa660c7b206d2f819f6bbe94808fafc86
cuong75201 <cuong75201@gmail.com>
2025-03-20 08:57:17 +0700

    fix navbar

--- mvc/views/inc/navbar.php
+++ mvc/views/inc/navbar.php
@@ -12,50 +12,50 @@
                             <ul class="vertical-menu-list">
                                 <li class="liChild">
                                     <a href="#">
-                                        <img src="../data/logo-banner/s2.jpg" alt="">
+                                        <img src="media//logo-banner/s2.jpg" alt="">
                                         <span>SÁCH MẦM NON</span>
                                     </a>
                                 </li>
                                 <li class="liChild">
                                     <a href="#">
-                                        <img src="../data/logo-banner/s3.jpg" alt="">
+                                        <img src="media//logo-banner/s3.jpg" alt="">
                                         <span>SÁCH THIẾU NHI</span>
                                     </a>
                                 </li>
                                 <li class="liChild">
                                     <a href="#">
-                                        <img src="../data/logo-banner/s4.jpg" alt="">
+                                        <img src="media//logo-banner/s4.jpg" alt="">
                                         <span>SÁCH KĨ NĂNG</span>
                                     </a>
                                 </li>
                                 <li class="liChild">
                                     <a href="#">
-                                        <img src="../data/logo-banner/s5.jpg" alt="">
+                                        <img src="media//logo-banner/s5.jpg" alt="">
                                         <span>SÁCH KINH DOANH</span>
                                     </a>
                                 </li>
                                 <li class="liChild">
                                     <a href="#">
-                                        <img src="../data/logo-banner/s6.jpg" alt="">
+                                        <img src="media//logo-banner/s6.jpg" alt="">
                                         <span>SÁCH MẸ VÀ BÉ</span>
                                     </a>
                                 </li>
                                 <li class="liChild">
                                     <a href="#">
-                                        <img src="../data/logo-banner/s7.jpg" alt="">
+                                        <img src="media//logo-banner/s7.jpg" alt="">
                                         <span>SÁCH VĂN HỌC</span>
                                     </a>
                                 </li>
                                 <li class="liChild">
                                     <a href="#">
-                                        <img src="../data/logo-banner/s8.jpg" alt="">
+                                        <img src="media//logo-banner/s8.jpg" alt="">
                                         <span>SÁCH THAM KHẢO</span>
                                     </a>
                                 </li>
                                 <li class="liChild">
                                     <a href="#">
-                                        <img src="../data/logo-banner/s9.jpg" alt="">
-                                        <span>NOTEBOOL</span>
+                                        <img src="media//logo-banner/s9.jpg" alt="">
+                                        <span>NOTEBOOK</span>
                                     </a>
                                 </li>
                             </ul>
@@ -64,15 +64,15 @@
                 </div>
                 <div class="col-10 d-flex navbar-menu">
                     <div class="item">
-                        <img src="../data/logo-banner/ship.png" alt="">
+                        <img src="media//logo-banner/ship.png" alt="">
                         <p>Ship COD Trên Toàn Quốc</p>
                     </div>
                     <div class="item">
-                        <img src="../data/logo-banner/freeship.png" alt="">
+                        <img src="media//logo-banner/freeship.png" alt="">
                         <p>Free Ship Đơn Hàng Trên 300k</p>
                     </div>
                     <div class="item">
-                        <img src="../data/logo-banner/support_phone.png" alt="">
+                        <img src="media//logo-banner/support_phone.png" alt="">
                         <p>0966160925/ 0989 849 396</p>
                     </div>
                 </div>
